3acceaa448f508e080263c62403fdb3e6a36f5e2
anastafj197 <frankie.anastasia@gmail.com>
2018-10-13 17:54:11 -0400

    Binding

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // One view instance to control entire app
 new Vue({
-  //import div id from index 
+  // import div id from index 
+  // Vuejs takes all properties in data object 
+  // Proxies them to the top of the Vue instance 
   el:'#vue-app',
 	data:{
-	  name: 'Banksy' 
+	  name: 'Banksy', 
+	  job:  'Artist',
+	  website: 'https://www.linkedin.com/in/frances-anastasia-84349711a/',
+	  websiteTag: '<a href="https://www.linkedin.com/in/frances-anastasia-84349711a/">Linkedin</a>'
+  },
+  methods: {
+  	greet: function(time){
+  		return 'Good ' + time + ' ' + this.name;
+  	}
   }
 });
