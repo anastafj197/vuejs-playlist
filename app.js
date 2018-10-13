ef099643ecdbb691b2c918b5ffd605273c78411e
anastafj197 <frankie.anastasia@gmail.com>
2018-10-13 18:40:21 -0400

    Events
    Added age increment / decrements  
    And mouse coordinate tracker

--- app.js
+++ app.js
@@ -7,12 +7,26 @@ new Vue({
 	data:{
 	  name: 'Banksy', 
 	  job:  'Artist',
+	  age: 23,
+	  x: 0,
+	  y: 0,
 	  website: 'https://www.linkedin.com/in/frances-anastasia-84349711a/',
 	  websiteTag: '<a href="https://www.linkedin.com/in/frances-anastasia-84349711a/">Linkedin</a>'
   },
   methods: {
   	greet: function(time){
   		return 'Good ' + time + ' ' + this.name;
+  	},
+  	add: function(inc){
+  		this.age += inc;
+  	},
+  	subtract: function(dec){
+  		this.age -= dec;
+  	},
+  	updateXY: function(event){
+  		
+  		this.x = event.offsetX;
+  		this.y = event.offsetY;
   	}
   }
 });
