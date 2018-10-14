09323786affb0044e55f2ee8b11d0e962920c6e2
anastafj197 <frankie.anastasia@gmail.com>
2018-10-14 14:35:56 -0400

    Added computed:
    Computed values are automatically updated when the Vue is instantiated.
    Very useful when manipulating data

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // One view instance to control entire app
 new Vue({
   // import div id from index 
-// One view instance to control entire app
-new Vue({
-  // import div id from index 
   // Vuejs takes all properties in data object 
   // Proxies them to the top of the Vue instance 
   el:'#vue-app',
 	data:{
 	  name: 'Banksy', 
 	  job:  'Artist',
+	  newAge: 20,
 	  age: 23,
+	  a: 0,
+	  b: 0,
 	  x: 0,
 	  y: 0,
 	  website: 'https://www.linkedin.com/in/frances-anastasia-84349711a/',
@@ -27,7 +27,6 @@ new Vue({
   		this.age -= dec;
   	},
   	updateXY: function(event){
-  		
   		this.x = event.offsetX;
   		this.y = event.offsetY;
   	},
@@ -39,6 +38,16 @@ new Vue({
   	},
   	logAge: function(){
   		console.log("You entered your age");
+  	}
+  },
+  computed: {
+  	addToA: function(){
+  		console.log('addToA');
+  		return this.a + this.newAge;
+  	},
+  	addToB: function(){
+  		console.log('addToB');
+  		return this.b + this.newAge;
   	}
   }
 });
