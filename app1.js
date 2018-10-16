1f3238b1868b61556e5e3b5ba7999ffc5f1c7039
anastafj197 <frankie.anastasia@gmail.com>
2018-10-15 20:37:21 -0400

    Added array and some data

--- app1.js
+++ app1.js
@@ -1,8 +1,16 @@
 new Vue({
 	el: '#vue-app',
 	data: {
+		characters: ['Sven','Riki','Zues','Drow'],
+		things: [
+			{name:'Creep', dps:'10'},
+			{name:'Tower', dps:'75'},
+			{name:'Ancient', dps:'250'}
+		],		
 		available: true,
-		nearby: false
+		nearby: false,
+		error: false,
+		sucess: false
 	},
 	methods: {
 
